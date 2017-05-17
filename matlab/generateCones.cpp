bcde62a7d4520c7dd6af8b422202bf5d22f29416
Okai Addy <noaddy@stanford.edu>
2017-05-17 09:05:53 -0700

    mex file runs

--- matlab/generateCones.cpp
+++ matlab/generateCones.cpp
@@ -1,6 +1,29 @@
+extern "C" 
+{
+#include "cones.h"
+}
+
 #include <mex.h>
+#include <math.h>
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
+	const double *input;
+
+	input = mxGetPr(prhs[0]);
+	float fieldOfViewXY = input[0];
+	float fieldOfViewZ = input[1];
+	input = mxGetPr(prhs[1]);
+	float spatialResolutionXY = input[0];
+	float spatialResolutionZ = input[1];
+	float readoutDuration = *mxGetPr(prhs[2]);
+
+	float filterFieldOfView = fmaxf(fieldOfViewXY, fieldOfViewZ);
+
+	float samplingInterval = 4e-6;
+	float maxGradientAmplitude = 4;
+	float maxSlewRate = 15000;
+
+	generateCones(fieldOfViewXY, fieldOfViewZ, NULL, spatialResolutionXY, spatialResolutionZ, 16, 1, NoCompensation, readoutDuration, samplingInterval, filterFieldOfView, maxGradientAmplitude, maxSlewRate, StoreAll);
 }
 
