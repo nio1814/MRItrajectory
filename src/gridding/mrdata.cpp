663a54fa890d425c41d9123404cdcb2c10ba57a7
Okai Addy <noaddy@stanford.edu>
2017-05-17 15:20:54 -0700

    initial mrdata

---
+++ src/gridding/mrdata.cpp
@@ -0,0 +1,7 @@
+#include "mrdata.h"
+
+MRdata::MRdata(std::vector<int> dimensions, int numImagingDimensions, std::vector<complexFloat> data) : m_numImagingDimensions(numImagingDimensions)
+{
+
+}
+
---
+++ src/gridding/mrdata.h
@@ -0,0 +1,19 @@
+#ifndef MRDATA_H
+#define MRDATA_H
+
+#include <vector>
+#include <complex>
+
+typedef std::complex<float> complexFloat;
+
+class MRdata
+{
+public:
+	MRdata(std::vector<int> dimensions, int numImagingDimensions, std::vector<complexFloat> data);
+private:
+	int m_numImagingDimensions;
+	std::vector<int> m_dimensions;
+	std::vector<complexFloat> m_signal;
+};
+
+#endif // MRDATA_H
