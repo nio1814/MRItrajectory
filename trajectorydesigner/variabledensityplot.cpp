60e79a146364f1d5ef2aba93269b38f88911a7f3
Okai Addy <noaddy@stanford.edu>
2017-10-14 21:37:12 -0700

    nearest point detection

--- trajectorydesigner/variabledensityplot.cpp
+++ trajectorydesigner/variabledensityplot.cpp
@@ -1,57 +1,108 @@
 #include "variabledensityplot.h"
 
 #include <QPainter>
+#include <QMouseEvent>
+#include <QDebug>
 
-VariableDensityPlot::VariableDensityPlot(QWidget *parent) : QWidget(parent)
+extern "C"
 {
+#include "variabledensity.h"
+}
+
+VariableDensityPlot::VariableDensityPlot(QWidget *parent) : QWidget(parent),
+	m_variableDensity(newVariableDensity())
+{
+	setFocus();
 	setAutoFillBackground(true);
 
 	QPalette palette;
 	palette.setColor(QPalette::Background, Qt::black);
 	setPalette(palette);
+
+	addPoint(0, 1);
+	addPoint(1, 1);
 }
 
 void VariableDensityPlot::paintEvent(QPaintEvent *event)
 {
 	Q_UNUSED(event);
 
-	QPen pen;
+	QPen penGridLine;
 	int gridLines = 5;
 	for(int n=0; n<gridLines+1; n++)
 	{
 		if(n)
 		{
-			pen.setColor(Qt::gray);
-			pen.setStyle(Qt::DashLine);
+			penGridLine.setColor(Qt::gray);
+			penGridLine.setStyle(Qt::DashLine);
 		}
 		else
 		{
-			pen.setColor(Qt::white);
-			pen.setStyle(Qt::SolidLine);
+			penGridLine.setColor(Qt::white);
+			penGridLine.setStyle(Qt::SolidLine);
 		}
 		float r = n/(float)gridLines;
-		drawLine(QPointF(r,0), QPointF(r,1), pen);
-		drawLine(QPointF(0,r), QPointF(1,r), pen);
+		drawLine(QPointF(r,0), QPointF(r,1), penGridLine);
+		drawLine(QPointF(0,r), QPointF(1,r), penGridLine);
 	}
 
 	QPainter painter(this);
 
-	pen.setColor(Qt::yellow);
-	pen.setWidth(5);
-	pen.setStyle(Qt::SolidLine);
-	painter.setPen(pen);
+	QPen penLine;
+	penLine.setColor(Qt::yellow);
+	penLine.setWidth(4);
+
+	QPen penPoint;
+	penPoint.setWidth(5);
+
 	QLinearGradient gradient;
 	painter.setBrush(gradient);
 	QPointF pointPrevious;
-	for(int n=0; n<m_points.size(); n++)
+	QPolygonF points;
+	for(int n=0; n<m_variableDensity->steps; n++)
 	{
-		QPainterPath path;
-		QPointF pointCurrent = mapToWindow(m_points[n]);
-		path.addEllipse(pointCurrent, 5, 5);
-		painter.drawPath(path);
+		QPointF point(m_variableDensity->step[n].kr, m_variableDensity->step[n].scale);		QPointF pointCurrent = mapToWindow(point);
+		points.append(pointCurrent);
 		if(n)
+		{
+			painter.setPen(penLine);
 			painter.drawLine(pointPrevious, pointCurrent);
+		}
 		pointPrevious = pointCurrent;
+	}
+	for(int n=0; n<m_variableDensity->steps; n++)
+	{
+		if(n==m_hoverIndex)
+			penPoint.setColor(Qt::red);
+		else
+			penPoint.setColor(Qt::yellow);
+		QPainterPath path;
+		path.addEllipse(points.at(n), 4, 4);
+		painter.setPen(penPoint);
+		painter.drawPath(path);
+	}
+}
+
+void VariableDensityPlot::mouseMoveEvent(QMouseEvent *event)
+{
+	QPointF cursorPoint = event->pos();
+//	qWarning() << cursorPoint;
+
+	m_hoverIndex = None;
+	for(int n=0; n<m_variableDensity->steps; n++)
+	{
+		const struct VariableDensityStep* step = &m_variableDensity->step[n];
+		QPointF variableDensityPoint = mapToWindow(QPointF(step->kr, step->scale));
+		QPointF offset = cursorPoint - variableDensityPoint;
+		float distance = offset.manhattanLength();
+
+		if(distance<10)
+		{
+			m_hoverIndex = n;
+//			qWarning() << distance;
+			update();
+			break;
+		}
 	}
 }
 
@@ -73,6 +124,11 @@ void VariableDensityPlot::drawLine(QPointF point1, QPointF point2, const QPen& p
 	QPainter painter(this);
 	painter.setPen(pen);
 	painter.drawLine(point1, point2);
+}
+
+void VariableDensityPlot::addPoint(float kr, float scale)
+{
+	addLinearVariableDensityStep(m_variableDensity, kr, scale);
 }
 
 
--- trajectorydesigner/variabledensityplot.h
+++ trajectorydesigner/variabledensityplot.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 
+QT_FORWARD_DECLARE_STRUCT(VariableDensity)
+
 class VariableDensityPlot : public QWidget
 {
 	Q_OBJECT
@@ -14,11 +16,17 @@ signals:
 public slots:
 protected:
 	void paintEvent(QPaintEvent *event);
+	void mouseMoveEvent(QMouseEvent *event);
 private:
+	enum Indices{None=-1};
+
 	QPointF mapToWindow(QPointF point);
 	void drawLine(QPointF point1, QPointF point2, const QPen& pen);
+	void addPoint(float kr, float scale);
 
 	QVector<QPointF> m_points = {{0,1}, {1,1}};
+	VariableDensity* m_variableDensity = NULL;
+	int m_hoverIndex = None;
 };
 
 #endif // VARIABLEDENSITYPLOT_H
