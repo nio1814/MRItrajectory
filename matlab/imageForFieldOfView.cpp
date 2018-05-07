7350236e14c4e5b5c04ad2ff291f8123ef9ef135
Okai Addy <noaddy@stanford.edu>
2018-05-06 19:30:59 -0700

    starting shepp logan mex

---
+++ matlab/imageForFieldOfView.cpp
@@ -0,0 +1,7 @@
+#include <mex.h>
+
+#include "phantom.h"
+
+mxArray* arrayToMxArray(void* array, mxClassID classID, mxComplexity complexity, std::vector<mwSize> size)
+{
+}
---
+++ matlab/test/shepplogan3D_test.m
@@ -0,0 +1,2 @@
+simulator = PhantomSimulator(3);
+m = simulator.imageForFieldOfView([28,28,14], [234,234,112]);
