d205d2f56f45a88a6ccf35ea1ce1d9941d23c563
Okai Addy <niioaddy@live.com>
2020-07-19 19:19:09 -0700

    can access trajectory as numpy

---
+++ python/pyside/numpy.h
@@ -0,0 +1,22 @@
+#ifndef NUMPY_H
+#define NUMPY_H
+
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
+#include <Python.h>
+#include <numpy/arrayobject.h>
+
+/*!
+ * \brief Set up code for using the NumPy API.
+ */
+void* initializeNumpy()
+{
+  Py_Initialize();
+  if (PyArray_API == NULL) {
+    import_array();
+  }
+
+  return NULL;
+}
+
+#endif // NUMPY_H
