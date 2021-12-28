1df4d40c92e671aefdeda2fb8514f42596f99404
Okai Addy <niioaddy@live.com>
2021-12-28 09:10:22 -0500

    initial command line generator

---
+++ trajectory/commandline/main.cpp
@@ -0,0 +1,58 @@
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QDebug>
+
+#include "trajectorygenerator.h"
+
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication application(argc, argv);
+
+  QCommandLineParser parser;
+  parser.addHelpOption();
+  parser.addPositionalArgument("trajectory", "The type of trajectory to generate");
+
+  QCommandLineOption readoutDuration("readdur", "Readout duration", "readoutDuration");
+  parser.addOption(readoutDuration);
+
+  QCommandLineOption fieldOfView("fov", "Field of view", "fieldOfView");
+  parser.addOption(fieldOfView);
+
+  parser.process(application);
+
+  if(parser.positionalArguments().empty())
+  {
+    qCritical("No arguments specified");
+    return 1;
+  }
+
+  TrajectoryType trajectoryType;
+
+  const QString trajectoryTypeRequested = parser.positionalArguments()[0];
+  if(!trajectoryTypeRequested.compare("cones", Qt::CaseInsensitive))
+     trajectoryType = CONES;
+  else
+    qCritical("Invalid trajectory type %s", qPrintable(trajectoryTypeRequested));
+
+  TrajectoryGenerator generator;
+  generator.setTrajectoryType(trajectoryType);
+  if(parser.isSet(readoutDuration))
+    generator.setReadoutDuration(parser.value(readoutDuration).toFloat());
+
+  if(parser.isSet(fieldOfView))
+  {
+    const QStringList fieldsOfView = parser.values(fieldOfView);
+    qDebug() << fieldsOfView;
+    if (trajectoryType == CONES)
+    {
+      const float fieldOfViewXY = fieldsOfView[0].toFloat();
+      const float fieldOfViewZ = fieldsOfView[1].toFloat();
+      generator.setFieldOfView({fieldOfViewXY, fieldOfViewXY, fieldOfViewZ});
+    }
+  }
+
+  generator.generate();
+
+  return 0;
+}
