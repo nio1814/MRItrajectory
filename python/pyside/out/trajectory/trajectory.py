a54d96030d2b1052b26827ac7c4c37cf0db37411
Okai Addy <niioaddy@live.com>
2021-12-31 09:09:11 -0500

    cones file loading

--- python/pyside/out/trajectory/trajectory.py
+++ python/pyside/out/trajectory/trajectory.py
@@ -1,4 +1,4 @@
-from enum import auto, Enum
+from enum import Enum
 
 import numpy as np
 
@@ -16,15 +16,17 @@ def read_array(file, data_type) -> np.ndarray:
   return np.fromfile(file, data_type, num_points)
 
 class TrajectoryType(Enum):
-  SPIRAL = auto()
-  CONES = auto()
+  SPIRAL = 'spiral'
+  RADIAL = 'radial'
+  RADIAL3D = 'radial3d'
+  CONES = 'cones'
 
   @classmethod
-  def from_index(cls):
+  def from_index(cls, index):
     return list(cls)[index]
 
 class Trajectory:
-  def __init__(self, file_path) -> None:
+  def __init__(self, file_path):
       with open(file_path, 'rb') as file:
         version = np.fromfile(file, 'i', 1)[0]
         self.type = TrajectoryType.from_index(np.fromfile(file, 'i', 1)[0])
@@ -34,12 +36,12 @@ class Trajectory:
         self.field_of_view = np.fromfile(file, 'f', num_dimensions)
         num_readouts = np.fromfile(file, 'i', 1)[0]
         num_bases = np.fromfile(file, 'i', 1)[0]
-        max_gradient_amplitude = np.fromfile(file, 'f', 1)[0]
-        max_readout_gradient_amplitude = np.fromfile(file, 'f', 1)[0]
-        max_slew_rate = np.fromfile(file, 'f', 1)[0]
+        self.max_gradient_amplitude = np.fromfile(file, 'f', 1)[0]
+        self.max_readout_gradient_amplitude = np.fromfile(file, 'f', 1)[0]
+        self.max_slew_rate = np.fromfile(file, 'f', 1)[0]
         num_waveform_points = np.fromfile(file, 'i', 1)[0]
         num_readout_points = np.fromfile(file, 'i', 1)[0]
-        sampling_interval = np.fromfile(file, 'f', 1)[0]
+        self.sampling_interval = np.fromfile(file, 'f', 1)[0]
         storage = np.fromfile(file, 'i', 1)[0]
 
         self.gradient_waveforms = read_array(file, 'f').reshape([num_readouts, 3, num_waveform_points])
@@ -47,17 +49,20 @@ class Trajectory:
         self.k_space_coordinates = read_array(file, 'f').reshape([num_readouts, 3, num_readout_points])
         self.density_compensation = read_array(file,  'f').reshape([num_readouts, num_readout_points])
 
+        if self.type == TrajectoryType.CONES:
+          self.cones = Cones(file)
+
 
 class Cones:
   def __init__(self, file) -> None:
-    cones_version = np.fromfile(file, 'i', 1)[0]
+    version = np.fromfile(file, 'i', 1)[0]
 
     rotatable = np.fromfile(file, 'i', 1)[0]
-    interconeCompensation = np.fromfile( file, 'i', 1)[0]
-    coneAngles = read_array('f')
-    coneAngleDensityCompensation = read_array('f')
-    basisConeAngles = read_array('f')
-    numBasisReadoutPoints = read_array('i')
-    numBasisWaveformPoints = read_array('i')
-    basisGradientWaveforms = read_array('f')
-    basisKspaceCoordinates = read_array('f')
+    self.intercone_compensation = np.fromfile( file, 'i', 1)[0]
+    self.cone_angles = read_array(file, 'f')
+    self.cone_angle_density_compensation = read_array(file, 'f')
+    self.basis_cone_angles = read_array(file, 'f')
+    self.num_basis_readout_points = read_array(file, 'i')
+    self.num_basis_waveform_points = read_array(file, 'i')
+    self.basis_gradient_waveforms = read_array(file, 'f')
+    self.basis_k_space_coordinates = read_array(file, 'f')
