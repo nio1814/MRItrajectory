4fe7a7366ea044a4f3da61e089cd7bbddede4e41
Okai Addy <noaddy@heartvista.com>
2022-01-11 00:46:14 -0500

    numpy array creation

---
+++ python/pyside/numpy.cpp
@@ -0,0 +1,18 @@
+#include "numpy.h"
+
+
+PyObject* mapNumpyArray(void *data, std::vector<npy_intp> dimensions)
+{
+  initializeNumpy();
+
+  std::vector<npy_intp> strides = {};
+  npy_intp stride = 1;
+  for (const npy_intp dimension : dimensions) {
+    strides.push_back(stride);
+    stride *= dimension;
+  }
+
+  std::reverse(std::begin(dimensions), std::end(dimensions));
+
+  return PyArray_NewFromDescr(&PyArray_Type, PyArray_DescrFromType(NPY_FLOAT32), dimensions.size(), dimensions.data(), NULL, data, NPY_ARRAY_WRITEABLE, NULL);
+}
